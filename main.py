8d2f68a50380f9697614603a06f05cb75559a951
AurisBut <67857119+AurisBut@users.noreply.github.com>
2021-01-22 14:58:00 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+import random
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
+            'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+
+# CARD Class (suit, rank, value)
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+class Deck:
+    def __init__(self):
+        self.all_cards = []
+        for suit in suits:
+            for rank in ranks:
+                # Create the Card Object
+                created_card = Card(suit, rank)
+                self.all_cards.append(created_card)
+
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        return self.all_cards.pop()
+
+new_deck = Deck()
+first_card = new_deck.all_cards[0]
+bottom_card = new_deck.all_cards[-1]
+print(first_card)
+print(bottom_card)
+new_deck.shuffle()
+first_card = new_deck.all_cards[0]
+bottom_card = new_deck.all_cards[-1]
+print(first_card)
+print(bottom_card)
+
+# two_hearts = Card(suits[0], ranks [0])
+# three_diamonds = Card(suits[1], ranks[1])
+# print(two_hearts)
+# print(two_hearts.value == three_diamonds.value)
