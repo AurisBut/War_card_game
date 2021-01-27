b78a9d5285c68e767a4c07393c705d549df7a7d8
AurisBut <67857119+AurisBut@users.noreply.github.com>
2021-01-27 07:02:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,18 +29,55 @@ class Deck:
     def deal_one(self):
         return self.all_cards.pop()
 
-new_deck = Deck()
-first_card = new_deck.all_cards[0]
-bottom_card = new_deck.all_cards[-1]
-print(first_card)
-print(bottom_card)
-new_deck.shuffle()
-first_card = new_deck.all_cards[0]
-bottom_card = new_deck.all_cards[-1]
-print(first_card)
-print(bottom_card)
+class Player:
+    def __init__(self,name):
+        self.name = name
+        self.all_cards = []
 
-# two_hearts = Card(suits[0], ranks [0])
-# three_diamonds = Card(suits[1], ranks[1])
-# print(two_hearts)
-# print(two_hearts.value == three_diamonds.value)
+    def remove_one(self):
+        return self.all_cards.pop(0)
+
+    def add_cards(self,new_cards):
+        if type(new_cards) == type([]):
+            # List of multiple Card objects
+            self.all_cards.extend(new_cards)
+        else:
+            # For a single Card object
+            self.all_cards.append(new_cards)
+
+    def __str__(self):
+        return f'Player {self.name} has {len(self.all_cards)} cards.'
+
+# GAME SETUP
+player_one = Player('One')
+player_two = Player('Two')
+new_deck = Deck()
+new_deck.shuffle()
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+game_on = True
+round_num = 0
+# Game on
+while game_on:
+    round_num += 1
+    print(f'Round {round_num}')
+
+    if len(player_one.all_cards) == 0:
+        print('Player One, out of cards! Player Two Wins!')
+        game_on = False
+        break
+    if len(player_two.all_cards) == 0:
+        print('Player Two, out of cards! Player One Wins!')
+        game_on = False
+        break
+
+    # START A NEW ROUND
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
+
+
+
